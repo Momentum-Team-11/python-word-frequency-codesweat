65a3875870ebbf1dc5338f958966b23d8f95f5bd
KarenElaine <karenelaine.stewart@yahoo.com>
2022-02-23 13:43:40 -0500

    added something

--- word_frequency.py
+++ word_frequency.py
@@ -5,9 +5,39 @@ STOP_WORDS = [
 ]
 
 
+f = open("alice_quote.txt", "r")
+
+print(f.read())
+
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
-    pass
+    with open(file) as file:
+        lines = file.read()
+    # how do we make this string into a list of words?
+    lines = lines.split()  # turns the string into a list of words
+    # We can count words now
+    count_words(lines)
+
+
+def count_words(lines):
+    print("len:", len(lines))
+    # how can we take this list and turn it into a dictionary that tracks occurences of each word?
+    # create a dictionary
+    word_count = {}
+    # take the list and add each word to the dictionary as a key
+    # and record the occurence of the word
+    # How could we get the words from the list into the dict as keys?
+    # we could do a for loop using the list!
+    for word in lines:
+        print(word)
+        # now that I have each word, I can add it to the dict as a key
+
+        # if word in word_count:
+        # increment the value by 1
+        # else:
+        # add key and value pair with the word as the key, and set the value to 1
+        word_count[word] = 1
+    print(word_count)
 
 
 if __name__ == "__main__":
@@ -15,8 +45,9 @@ if __name__ == "__main__":
     from pathlib import Path
 
     parser = argparse.ArgumentParser(
-        description='Get the word frequency in a text file.')
-    parser.add_argument('file', help='file to read')
+        description="Get the word frequency in a text file."
+    )
+    parser.add_argument("file", help="file to read")
     args = parser.parse_args()
 
     file = Path(args.file)
