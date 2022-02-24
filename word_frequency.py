291e9d2221d9eb5c6b2ec21b05c9fdaab25eb581
KarenElaine <karenelaine.stewart@yahoo.com>
2022-02-23 20:56:55 -0500

    removed punctuation, made words lowercase, put words in a list

--- word_frequency.py
+++ word_frequency.py
@@ -1,23 +1,34 @@
+import string
+from tkinter import filedialog
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
-
-f = open("alice_quote.txt", "r")
-
-print(f.read())
-
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     with open(file) as file:
-        lines = file.read()
+        word_list = file.read()
     # how do we make this string into a list of words?
-    lines = lines.split()  # turns the string into a list of words
+    word_list = word_list.lower()  # turns the string into a list of words
+    word_list = word_list.translate(str.maketrans('', '', string.punctuation))
+    word_list = word_list.split()
     # We can count words now
-    count_words(lines)
-
+    # count_words(word_list)
+    for word in word_list:
+        if word in STOP_WORDS:
+            word_list.remove(word)
+           
+# def f(words):
+#     wdict = {}
+#     for word in words:
+#         if word not in wdict:
+#             wdict[word] = 0
+#         wdict[word] += 1
+#     return wdict
 
 def count_words(lines):
     print("len:", len(lines))
@@ -29,15 +40,18 @@ def count_words(lines):
     # How could we get the words from the list into the dict as keys?
     # we could do a for loop using the list!
     for word in lines:
-        print(word)
+        return word
+        #print(word)
         # now that I have each word, I can add it to the dict as a key
 
         # if word in word_count:
         # increment the value by 1
         # else:
         # add key and value pair with the word as the key, and set the value to 1
-        word_count[word] = 1
-    print(word_count)
+    #     word_count[word] = 1
+    # print(word_count)
+
+
 
 
 if __name__ == "__main__":
